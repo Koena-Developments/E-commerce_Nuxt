7b70764a748b284f7493fc5763624b251cc16a7c
Thabang Motswenyane <thabangmotswenyane511@gmail.com>
2025-05-20 16:43:24 +0200

    with instructions

--- components/viewProduct.vue
+++ components/viewProduct.vue
@@ -6,6 +6,13 @@
 </template>
 
 <script setup>
+/*
+  I want to receive a prop from the parent class(index) 
+  I want to use those product to render the data in this view/Page  
+
+*/
+
+
 const {product} = defineProps(['product']) 
 </script>
 
