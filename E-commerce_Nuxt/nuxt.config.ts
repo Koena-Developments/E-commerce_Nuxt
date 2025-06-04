69868b4ccd9c670271e724d43d241801ecd932ba
Your Name <you@example.com>
2025-06-04 05:01:28 +0200

    progress

--- E-commerce_Nuxt/nuxt.config.ts
+++ E-commerce_Nuxt/nuxt.config.ts
@@ -5,19 +5,20 @@ export default defineNuxtConfig({
     '@fortawesome/fontawesome-free/css/all.min.css'
   ],
   modules: ['@nuxt/fonts', '@pinia/nuxt', '@sidebase/nuxt-auth'],
-  // auth: {
-  //  baseURL: process.env.AUTH_ORIGIN, 
-  // provider: {
-  //   type: 'authjs'
-  // }
-  // },
-  //   runtimeConfig: {
-  //   authSecret: process.env.NUXT_AUTH_SECRET,
-  //   public: {
-  //     apiBaseUrl: process.env.NUXT_API_BASE_URL,
-  //     authUrl: process.env.AUTH_ORIGIN
-  //   }
-  // }
-
-// '@sidebase/nuxt-auth'
+  auth: {
+    baseURL: process.env.NUXT_PUBLIC_AUTH_BASE_URL,
+    globalAppMiddleware: {
+      isEnabled: true
+    },
+    provider: {
+      type: 'authjs'
+    }
+  },
+  runtimeConfig: {
+    authSecret: process.env.NUXT_AUTH_SECRET,
+    public: {
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL,
+      authBaseUrl: process.env.NUXT_PUBLIC_AUTH_BASE_URL 
+    }
+  }
 })
--- E-commerce_Nuxt/server/api/auth/[...].ts
+++ E-commerce_Nuxt/server/api/auth/[...].ts
@@ -1,92 +1,93 @@
-// import CredentialsProvider from '@auth/credentials-provider'
-// import { NuxtAuthHandler } from '#auth'
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { NuxtAuthHandler } from '#auth'
 
-// export default NuxtAuthHandler({
-//   secret: useRuntimeConfig().authSecret,
+export default NuxtAuthHandler({
+  secret: useRuntimeConfig().authSecret,
   
-//   providers: [
-//     CredentialsProvider({
-//       name: 'credentials',
-//       credentials: {
-//         email: { label: 'Email', type: 'email' },
-//         password: { label: 'Password', type: 'password' }
-//       },
+  providers: [
+    // @ts-ignore:
+    CredentialsProvider.default({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' }
+      },
       
-//       async authorize(credentials) {
-//         try {
-//           const response = await $fetch('/auth/login', {
-//             method: 'POST',
-//             baseURL: useRuntimeConfig().apiBaseUrl,
-//             body: {
-//               email: credentials?.email,
-//               password: credentials?.password
-//             }
-//           })
+      async authorize(credentials) {
+        try {
+          const response = await $fetch('/auth/login', {
+            method: 'POST',
+            baseURL: useRuntimeConfig().apiBaseUrl,
+            body: {
+              email: credentials?.email,
+              password: credentials?.password
+            }
+          })
 
-//           if (response && response.status === true && response.result) {
-//             return {
-//               id: credentials?.email, 
-//               email: credentials?.email,
-//               name: credentials?.email?.split('@')[0], 
-//               accessToken: response.result.token,
-//               tokenExpires: response.result.expires
-//             }
-//           }
+          if (response && response.status === true && response.result) {
+            return {
+              id: credentials?.email, 
+              email: credentials?.email,
+              name: credentials?.email?.split('@')[0], 
+              accessToken: response.result.token,
+              tokenExpires: response.result.expires
+            }
+          }
           
 
-//           return null
-//         } catch (error) {
-//           console.error('Authentication error:', error)
+          return null
+        } catch (error) {
+          console.error('Authentication error:', error)
           
     
-//           if (error?.data?.status === false) {
-//             console.error('Login failed:', error.data.error)
-//           }
+          if (error?.data?.status === false) {
+            console.error('Login failed:', error.data.error)
+          }
           
-//           return null
-//         }
-//       }
-//     })
-//   ],
+          return null
+        }
+      }
+    })
+  ],
 
-//   callbacks: {
+  callbacks: {
     
-//     async jwt({ token, user }) {
+    async jwt({ token, user }) {
     
-//       if (user) {
-//         token.accessToken = user.accessToken
-//         token.tokenExpires = user.tokenExpires
-//         token.id = user.id
-//         token.email = user.email
-//         token.name = user.name
-//       }
+      if (user) {
+        token.accessToken = user.accessToken
+        token.tokenExpires = user.tokenExpires
+        token.id = user.id
+        token.email = user.email
+        token.name = user.name
+      }
       
-//       if (token.tokenExpires && new Date() > new Date(token.tokenExpires)) {
-//         return null
-//       }
+      if (token.tokenExpires && new Date() > new Date(token.tokenExpires)) {
+        return null
+      }
       
-//       return token
-//     },
+      return token
+    },
     
-//     async session({ session, token }) {
-//       if (token) {
-//         session.accessToken = token.accessToken
-//         session.user.id = token.id
-//         session.user.email = token.email
-//         session.user.name = token.name
-//         session.tokenExpires = token.tokenExpires
-//       }
-//       return session
-//     }
-//   },
+    async session({ session, token }) {
+      if (token) {
+        session.accessToken = token.accessToken
+        session.user.id = token.id
+        session.user.email = token.email
+        session.user.name = token.name
+        session.tokenExpires = token.tokenExpires
+      }
+      return session
+    }
+  },
 
-//   pages: {
-//     signIn: '/login',
-//     error: '/auth/error'
-//   },
+  pages: {
+    signIn: '/login',
+    error: '/auth/error'
+  },
 
-//   session: {
-//     strategy: 'jwt',
-//     maxAge: 24 * 60 * 60,
-//   }
-// })
+  session: {
+    strategy: 'jwt',
+    maxAge: 24 * 60 * 60,
+  }
+})
